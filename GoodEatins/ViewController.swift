75f6bfe203604541615695442574eaaf69791510
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-23 13:34:02 +0900

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by 薮本崇仁 on 2019/09/23.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

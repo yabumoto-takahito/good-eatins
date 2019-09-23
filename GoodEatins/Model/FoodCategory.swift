41a09d4230f231445bc8bf696387bada98d0cab5
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-23 14:44:14 +0900

    Data Set for GoodEatins
    GoodEatinsのデータをセット。

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,16 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by 薮本崇仁 on 2019/09/23.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+//  let：再代入不可能なプロパティ
+//  var：再代入可能なプロパティ
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by 薮本崇仁 on 2019/09/23.
+//  Copyright © 2019 薮本崇仁. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
